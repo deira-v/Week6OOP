bd75cefe07a6a6b53089d4ca1c9b228ecadb82f2
deira <deiravalci@gmail.com>
2025-10-22 16:52:40 +0100

    Read Example Added

--- src/main/java/ie/atu/WriteOnce.java
+++ src/main/java/ie/atu/WriteOnce.java
@@ -1,27 +1,22 @@
 package ie.atu;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Scanner;
 
 public class WriteOnce {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter the file name. (e.g., students.txt)");
+        System.out.println("Enter the file name: (e.g. student.txt");
         String fileName = sc.nextLine().trim();
 
-        System.out.println("Enter a name to save");
-        String name = sc.nextLine();
+        System.out.println("Enter a name to save:");
+        String name = sc.nextLine().trim();
 
         try (PrintWriter out = new PrintWriter(new FileWriter(fileName, true))) {
-
             out.println(name);
             System.out.println("Saved to: " + fileName);
-            //out.close();    //close the file and writ ethe information
         } catch (IOException ex) {
-            System.out.println("Could not save file." + ex.getMessage());
+            System.out.println("Could not write to: " + ex.getMessage());
         }
     }
 }
