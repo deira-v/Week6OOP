ab4e7e28f78399dbeae5544b8190f6673bb29f04
deira <deiravalci@gmail.com>
2025-10-22 16:29:53 +0100

    Added WriteOnce with tread try/catch

--- src/main/java/ie/atu/WriteOnce.java
+++ src/main/java/ie/atu/WriteOnce.java
@@ -6,26 +6,22 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class WriteOnce {
-    public static void main(String[] args) throws IOException
-    {
-        Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the file name. (e.g., students.txt)");
         String fileName = sc.nextLine().trim();
 
         System.out.println("Enter a name to save");
         String name = sc.nextLine();
-        try
-        {
-            PrintWriter out = new PrintWriter(new FileWriter(fileName, true));
+
+        try (PrintWriter out = new PrintWriter(new FileWriter(fileName, true))) {
 
             out.println(name);
             System.out.println("Saved to: " + fileName);
-            out.close();    //close the file and writ ethe information
-        }
-        catch (IOException ex)
-        {
-            System.out.println("Could not save file." = ex.getMessage());
+            //out.close();    //close the file and writ ethe information
+        } catch (IOException ex) {
+            System.out.println("Could not save file." + ex.getMessage());
         }
     }
 }
